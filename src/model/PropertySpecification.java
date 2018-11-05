9f8f5fa32a80b9108bb4eaf7018ea98e8acfc52e
gdkapos <gdkapos@gmail.com>
2018-11-05 16:06:24 +0100

    Create PropertySpecification.java

---
+++ src/model/PropertySpecification.java
@@ -0,0 +1,12 @@
+public class PropertySpecification extends NamedVersionedElement {
+    private Type ownerType;
+    private Type propertyType;
+    
+    public Type getOwnerType() {
+        return this.ownerType;
+    }
+    
+public Type getPropertyType() {
+        return this.propertyType;
+    }
+}
