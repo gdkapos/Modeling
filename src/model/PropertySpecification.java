153727a0afbbfaa95e5a24c360bd42e07dee1f9e
gdkapos <gdkapos@gmail.com>
2018-11-05 17:10:17 +0100

    Update PropertySpecification.java

--- src/model/PropertySpecification.java
+++ src/model/PropertySpecification.java
@@ -1,12 +1,17 @@
 public class PropertySpecification extends NamedVersionedElement {
     private Type ownerType;
     private Type propertyType;
+    private Multiplicity multiplicity;
     
     public Type getOwnerType() {
         return this.ownerType;
     }
     
-public Type getPropertyType() {
+    public Type getPropertyType() {
         return this.propertyType;
+    }
+    
+    public Multiplicity getMultiplicity() {
+        return this.multiplicity;
     }
 }
