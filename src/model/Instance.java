c4a3c9574435d524a10a05e9dd86dcafba5cab64
gdkapos <gdkapos@gmail.com>
2018-11-05 17:06:56 +0100

    Update Instance.java

--- src/model/Instance.java
+++ src/model/Instance.java
@@ -1,3 +1,8 @@
 public class Instance extends NamedVersionedElement {
+    private Type type;
+    private List<ProperyValue> propertyValues;
     
+    public Tyle getType() {
+        return this.type;
+    }
 }
