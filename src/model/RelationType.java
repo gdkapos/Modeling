773e4a9ed78f052241392696a97c068a23421208
gdkapos <gdkapos@gmail.com>
2018-11-05 16:57:01 +0100

    Update RelationType.java

--- src/model/RelationType.java
+++ src/model/RelationType.java
@@ -1,3 +1,22 @@
 public class RelationType extends {
-    Type 
+    private Type relationOwnerType;
+    private Type relationPartType;
+    private Multiplicity relationOwnerMultiplicity;
+    private Multiplicity relationOwnerMultiplicity;
+    
+    public Type getRelationOwnerType() {
+        return this.relationOwnerType;
+    }
+    
+    public Type getRelationPartType() {
+        return this.relationPartType;
+    }
+    
+    public Multiplicity getRelationOwnerMultiplicity() {
+        return this.relationOwnerMultiplicity;
+    }
+    
+    public Multiplicity getRelationOwnerMultiplicity() {
+        return this.relationOwnerMultiplicity;
+    }
 }
